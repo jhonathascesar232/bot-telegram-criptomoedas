4e11fc791991ae4a60639b9491130bdf4259c259
Jhonathas César <jhonathascesar232@gmail.com>
2021-08-08 16:30:44 -0300

    editado urls.py

--- cotacaodecriptomoedasbot/urls.py
+++ cotacaodecriptomoedasbot/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import handle_bot_request, poll_updates, set_my_webhook
+from .views import handle_bot_request, poll_updates, set_my_webhook, delete_my_webhook
 
 urlpatterns = [
     path('update/', handle_bot_request),
